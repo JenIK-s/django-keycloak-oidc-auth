a748062db46eefe55244e45ac2a38b2d0b9de86b
jeniks <fertuv68@gmail.com>
2026-07-28 10:42:32 +0300

    update readme

---
+++ django_keycloak_oidc/config.py
@@ -0,0 +1,12 @@
+from django.conf import settings
+
+OIDC_RP_CLIENT_ID = settings.DKA_CLIENT_ID
+OIDC_RP_CLIENT_SECRET = settings.DKA_CLIENT_SECRET
+
+KC_BASE_URL = f"https://{settings.DKA_BASE_URL}/realms/{settings.DKA_REALM}/protocol/openid-connect"
+
+OIDC_OP_AUTHORIZATION_ENDPOINT = f"{KC_BASE_URL}/auth"
+OIDC_OP_TOKEN_ENDPOINT = f"{KC_BASE_URL}/token"
+OIDC_OP_USER_ENDPOINT = f"{KC_BASE_URL}/userinfo"
+OIDC_OP_JWKS_ENDPOINT = f"{KC_BASE_URL}/certs"
+OIDC_OP_LOGOUT_ENDPOINT = f"{KC_BASE_URL}/logout"
